65d627a2809a2ab81a225140edeac970e3339352
Bisultanova Tanzila <tanzbis81@gmail.com>
2021-10-04 21:44:48 +0300

    Difficult first homework is done

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ let end = result**3;
 console.log(end);
 console.log(String(end).slice(0, 2));
 
+let screens = "Простые, Сложные, Интерактивные".toLowerCase();
+console.log(screens.length);
+const forScreens = screens.split(", ");
+console.log(forScreens);
+
+
+
+
+
+
+
+if (request.status == 200) { // Убедиться в отсутствии ошибок
+  // Убедиться, что ответ  это XMLдокумент
+  if (request.getResponseHeader("Content-Type") == "text/xml") {
+  var doc = request.responseXML;
+  // Теперь обработать полученный документ
+  }
+ }
+
+
+
+
+
+
