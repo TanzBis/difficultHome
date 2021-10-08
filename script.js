f4d7c47f260fbb2e76736d3902a35c4cab74c378
Bisultanova Tanzila <tanzbis81@gmail.com>
2021-10-08 22:40:00 +0300

    Let it be correct

--- script.js
+++ script.js
@@ -1,38 +1,14 @@
 'use strict'
 
-
-let personName = "Артем";
-let result;
-
-result = (personName = "Артем") ? 'Директор' :
-  (personName = "Александр") ? 'Преподаватель' :
-  'студент';
-console.log(result);
-
-let lang = "en"; //Взависимости от того, что я тут пишу ru или en
-if (lang == "ru") {
-  console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-}else if (lang == "en") {
-  console.log("Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday");
-}else {
-  console.log('произошла ошибка');
-} 
-
-
-
-let lang = 'ru'; //Тут точно так же
-switch (lang) {
-  case "ru":
-    console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-    break;
-  case "en":
-    console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
-    break;
+function AnnyStr(a) {
+  if (typeof a != 'string') {
+  return "Вы ввели нестрочный тип";
+  }
+  if (a.length < 30) {
+  return a;
+  }
+  a = a.slice(0, 30) + '...';
+  return a;
 }
 
-var lang = 'en';
-var myArray = {
-	'ru':['понедельник', 'вторник', 'среда','четверг', 'пятницы', 'суббота', 'воскресенье'],
-	'en':['Sunday','Monday', 'Tuesday', 'Wensdey', 'Thursday', 'Friday', 'Saturday'],
-};
-console.log(myArray[lang]);
+console.log(AnnyStr("лороааа  аа а а аааалsыыыыыфыыыы"));
